0e5004dddd55511d7df30cdda4823f66881aea28
alvaroFGG <alvaro@mbp-de-alvaro.home>
2022-05-22 12:38:44 +0200

    finished page - later i'll fix/add features

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const pokedexSection$$ = document.querySelector("section[class = 'pokedex'");
+const search$$ = document.querySelector("input[class='headerPoke__input']");
 let OFFSET = 0;
+let BOOL = true;
 
 const elemsToDOM = (pokemon) =>{
+    
     const cardDiv$$ = document.createElement("div");
     cardDiv$$.classList.add("pokedex__card");
+    cardDiv$$.style.order = pokemon.order;
 
     const img = document.createElement("img");
     img.setAttribute("src",pokemon.sprites.front_default);
@@ -14,13 +18,52 @@ const elemsToDOM = (pokemon) =>{
     cardDiv$$.appendChild(img);
     cardDiv$$.appendChild(pokemonName$$);
     pokedexSection$$.appendChild(cardDiv$$);
+
 }
 
+const seeker = (allPokemons) =>{
+    const filteredPokemons = [];
+   
+    if(search$$.value!=''){
+        
+    
+        for(const pokemon of allPokemons){
+            
+            if(pokemon.name.toLowerCase().startsWith(search$$.value.toLowerCase().trim())){
+                filteredPokemons.push(pokemon);
+            }
+        }
+        pokedexSection$$.innerHTML = ``;
+        for(const pokemon of filteredPokemons){
+            elemsToDOM(pokemon); 
+        }
+    }else{
+        pokedexSection$$.innerHTML = ``;
+        OFFSET = 0;
+        fetchAPI();
+    }
+}
+
+search$$.addEventListener("input", async () =>{
+    const allPokemonsArr = [];
+    const apiData = await fetch('https://pokeapi.co/api/v2/pokemon?limit=150');
+    const allApiData = await apiData.json();
+    
+    for(each of allApiData.results){
+        const fetchPokemon = await fetch(each.url);
+        const pokemon = await fetchPokemon.json();
+        
+        allPokemonsArr.push(pokemon);
+
+    }
+    seeker(allPokemonsArr);
+});
 
 const fetchEachElem = (url) => {
     fetch(url)
     .then(response => response.json())
     .then(pokemon => {
+        // search$$.addEventListener("input", () => seeker(pokemon));
         elemsToDOM(pokemon);
     })
     .catch(error => {
@@ -39,6 +82,7 @@ const fetchAPI = () => {
             
             fetchEachElem(element.url);
         });
+        
     }).catch(error => {
         console.log(`La API no responde: ${error}`);
     });
